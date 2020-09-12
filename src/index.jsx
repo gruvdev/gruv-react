bf2113cc7df160807e72d2b48dcfd9f55135d5a2
Derek Shanks <dbshanks75@hotmail.com>
2020-09-12 00:29:42 -0300

    Initial config and commit

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Web } from "./Web/Web";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Web />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
